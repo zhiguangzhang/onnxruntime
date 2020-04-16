75a290af802840dae1eb57af0e1b4cef551052b1
Hector Li <hecli@microsoft.com>
2020-04-16 14:54:10 -0700

    explicitly set the environment variable CUDA_PATH for Windows GPU build

--- tools/ci_build/github/windows/setup_env_trt.bat
+++ tools/ci_build/github/windows/setup_env_trt.bat
@@ -1,2 +1,3 @@
 set PATH=C:\azcopy;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.2\bin;C:\local\cudnn-10.2-windows10-x64-v7.6.5.32\cuda\bin;%PATH%
 set GRADLE_OPTS=-Dorg.gradle.daemon=false
+set CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\%1
