1a3c1adb5f111e8dfdba62155aeee77902eb191e
Ye Wang <52801275+wangyems@users.noreply.github.com>
2020-04-23 13:31:04 -0700

    datetime_transformer work

--- onnxruntime/featurizers_ops/cpu/date_time_transformer.cc
+++ onnxruntime/featurizers_ops/cpu/date_time_transformer.cc
@@ -25,7 +25,7 @@ class DateTimeTransformer final : public OpKernel {
           const uint8_t* const state_data(state_tensor->Data<uint8_t>());
 
           Microsoft::Featurizer::Archive archive(state_data, state_tensor->Shape().Size());
-          return Microsoft::Featurizer::Featurizers::DateTimeTransformer(archive);
+          return Microsoft::Featurizer::Featurizers::DateTimeTransformer(archive, "C:\\Users\\Administrator\\Desktop\\ORT\\datetime\\build\\Windows\\RelWithDebInfo\\external\\FeaturizersLibrary\\");
         }());
 
     // Get the input
